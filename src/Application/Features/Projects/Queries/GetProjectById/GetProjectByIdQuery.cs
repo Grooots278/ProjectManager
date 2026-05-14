5542d2cb74e925ccc0ac057cdbd156bfd9257c80
Dmitry <dolphin1309@mail.com>
2026-05-14 22:46:26 +0700

    add get project by ud query

---
+++ src/Application/Features/Projects/Queries/GetProjectById/GetProjectByIdQuery.cs
@@ -0,0 +1,32 @@
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ProjectManager.Application.Common.Interfaces;
+using ProjectManager.Application.Features.Projects.DTOs;
+
+namespace ProjectManager.Application.Features.Projects.Queries.GetProjectById;
+
+public record GetProjectByIdQuery(Guid Id) : IRequest<ProjectDto?>;
+
+public class GetProjectByIdQueryHandler : IRequestHandler<GetProjectByIdQuery, ProjectDto?>
+{
+    private readonly IApplcationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetProjectByIdQueryHandler(IApplcationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<ProjectDto?> Handle(GetProjectByIdQuery request, CancellationToken cancellationToken)
+    {
+        var project = await _context.Projects
+            .Include(p => p.ProjectManager)
+            .Include(p => p.ProjectEmployees)
+                .ThenInclude(pe => pe.Employee)
+            .FirstOrDefaultAsync(p => p.Id == request.Id, cancellationToken);
+
+        return project is null ? null : _mapper.Map<ProjectDto>(project);
+    }
+}
