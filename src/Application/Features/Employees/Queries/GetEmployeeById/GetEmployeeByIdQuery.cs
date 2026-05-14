00857f1950b4b908bf8e21c913002ca03e7a8886
Dmitry <dolphin1309@mail.com>
2026-05-14 23:19:54 +0700

    Getting an employee by ID

---
+++ src/Application/Features/Employees/Queries/GetEmployeeById/GetEmployeeByIdQuery.cs
@@ -0,0 +1,26 @@
+using AutoMapper;
+using MediatR;
+using ProjectManager.Application.Common.Interfaces;
+using ProjectManager.Application.Features.Employees.Queries.GetEmployees;
+
+namespace ProjectManager.Application.Features.Employees.Queries.GetEmployeeById;
+
+public record GetEmployeeByIdQuery(Guid Id) : IRequest<EmployeeDto?>;
+
+public class GetEmployeeByIdQueryHandler : IRequestHandler<GetEmployeeByIdQuery, EmployeeDto?>
+{
+    private readonly IApplcationDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetEmployeeByIdQueryHandler(IApplcationDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<EmployeeDto?> Handle(GetEmployeeByIdQuery request, CancellationToken cancellationToken)
+    {
+        var employee = await _context.Employees.FindAsync(new object[] { request.Id }, cancellationToken);
+        return employee is null ? null : _mapper.Map<EmployeeDto>(employee);
+    }
+}
